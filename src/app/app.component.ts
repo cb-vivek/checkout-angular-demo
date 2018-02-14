8f31ef62256eb2497ffe0b190eb66179abf054a6
Vivekanandhan <vivek@chargebee.com>
2018-02-14 05:47:51 +0530

    Api example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,53 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  // button = "<button data-cb-type='checkout' data-cb-plan-id='cbdemo_grow' data-cb-addons[id][0]='cbdemo_additionaluser' data-cb-addons[quantity]='1'> subscribe </button>";
+  cbInstance;
+
+  constructor(private http: HttpClient){
+
+  }
+
   ngOnInit() {
-    document.addEventListener("DOMContentLoaded", () => {
-      let cbInstance = window['Chargebee'].getInstance();
-
-      // To add addons
-      // Get the element with the corresponding plan and addons
-      let planElement = document.querySelector("[data-cb-plan-id='cbdemo_grow']");
-      let product = cbInstance.getProduct(planElement);
-      product.addons.push({id: "cbdemo_additionaluser", quantity: 1});
-
-      // to add coupon
-      product.addCoupon("cbdemo_earlybird");
-
-      // adding subscription custom fields
-      product.data["cf_subtest"] = "subscription custom field";
-
-      // To add coupons and customer related information with custom fields
-      let cart = cbInstance.getCart();
-      // Date should be in YYYY-MM-DD
-      cart.setCustomer({email: "vivek@chargebee.com", cf_test: "customer custom field", cf_date: "1991-09-16"});
-
-      cbInstance.setCheckoutCallbacks(function(cart) {
-        // You can get the plan name for which the checkout happened like below
-        let product = cart.products[0];
-        console.log(product.planId);
-        console.log(product.addons);
-        return {
-            loaded: function() {
-                console.log("checkout opened");
-            },
-            close: function() {
-                console.log("checkout closed");
-            },
-            success: function(hostedPageId) {
-              console.log(hostedPageId);
-              // Hosted page id will be unique token for the checkout that happened
-              // You can pass this hosted page id to your backend 
-              // and then call our retrieve hosted page api to get subscription details
-              // https://apidocs.chargebee.com/docs/api/hosted_pages#retrieve_a_hosted_page
-            },
-            step: function(value) {
-                // value -> which step in checkout
-                console.log(value);
-            }
-        }
-      });
+    this.cbInstance = window['Chargebee'].init({
+      site: "vivek1-test"
     });
+    this.cbInstance.setPortalSession(() => {
+      // Hit your end point that returns portal session object as response
+      // This sample end point will call the below api
+      // https://apidocs.chargebee.com/docs/api/portal_sessions#create_a_portal_session
+      return this.http.post("https://www.recur.in/api/create_portal_session", {customer_id: 'cbdemo_sir'}).toPromise();
+    });
+  }
+
+  openCheckout() {
+    // Just for demo, Logging out the previous session, in the real scenario customer id / subscription id would be passed in checkout api
+    this.cbInstance.logout();
+    this.cbInstance.openCheckout({
+      hostedPage: () => {
+        // Hit your end point that returns hosted page object as response
+        // This sample end point will call the below api
+        // https://apidocs.chargebee.com/docs/api/hosted_pages#checkout_new_subscription
+
+        return this.http.post("https://www.recur.in/api/generate_hp_url", {plan_id: 'cbdemo_grow'}).toPromise();
+      },
+      loaded: function() {
+        console.log("checkout opened");
+      },
+      close: function() {
+          console.log("checkout closed");
+      },
+      success: function(hostedPageId) {
+        console.log(hostedPageId);
+        // Hosted page id will be unique token for the checkout that happened
+        // You can pass this hosted page id to your backend 
+        // and then call our retrieve hosted page api to get subscription details
+        // https://apidocs.chargebee.com/docs/api/hosted_pages#retrieve_a_hosted_page
+      },
+      step: function(value) {
+          // value -> which step in checkout
+          console.log(value);
+      }
+    });
+  }
+
+  openPortal() {
+    this.cbInstance.createChargebeePortal().open();
   }
 }
